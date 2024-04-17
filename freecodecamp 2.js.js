eb3ca00f674f12ffe1e8938b0986adcd981547e9
Brian Grifinn <briangrifinn@gmail.com>
2024-04-17 22:24:12 +0200

    Add files via upload
    Create another variable called currentWeapon and set it to 0.
    When a variable name has multiple words, the convention in 
    JavaScript is to use what's called camelCase. The first word is lowercase, and the first letter of every following word is uppercase.
    let thisIsCamelCase;

---
+++ freecodecamp 2.js.js
@@ -0,0 +1,6 @@
+/**Create another variable called currentWeapon and set it to 0.
+When a variable name has multiple words, the convention in 
+JavaScript is to use what's called camelCase. The first word is lowercase, and the first letter of every following word is uppercase.
+let thisIsCamelCase;**/
+
+let currentWeapon = 0;
