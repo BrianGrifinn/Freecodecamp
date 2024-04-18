66ad1120fb004b673c1973f3917f3e63d5dd5686
Brian Grifinn <briangrifinn@gmail.com>
2024-04-18 22:27:15 +0200

    Add files via upload
    In your role playing game, users will be able to track their stats, buy weapons, and fight monsters.
     Before you can continue with the interactive JavaScript portion of the game, you need to first create the HTML 
     elements that will display the game information.
    
    Create four div elements within your #game element. Give them the following respective id values,
     in order: stats, controls, monsterStats, and text.

---
+++ freecodecam8.js
@@ -0,0 +1,18 @@
+/**
+    Step 15
+In your role playing game, users will be able to track their stats, buy weapons, and fight monsters.
+ Before you can continue with the interactive JavaScript portion of the game, you need to first create the HTML 
+ elements that will display the game information.
+
+Create four div elements within your #game element. Give them the following respective id values,
+ in order: stats, controls, monsterStats, and text.
+**/
+
+<body>
+<div id="game">
+  <div id="stats"></div>
+  <div id="controls"></div>
+  <div id="monsterStats"></div>
+  <div id="text"></div>
+</div>
+</body>
