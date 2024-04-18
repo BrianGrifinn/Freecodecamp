a7dec5616cb998f3ad7f3848ff5a46d74eee304b
Brian Grifinn <briangrifinn@gmail.com>
2024-04-18 22:28:19 +0200

    Add files via upload
    Wrap the numbers 0, 100, and 50 in span elements, and wrap those new span elements in strong elements. Then give your new span elements id values of xpText, healthText, and goldText, respectively.
    
    Your answer should follow this basic structure:
    
    <span class="stat">TEXT <strong><span id="VALUE">TEXT</span></strong></span>

---
+++ freecodecamp 10.js
@@ -0,0 +1,12 @@
+/**
+    Wrap the numbers 0, 100, and 50 in span elements, and wrap those new span elements in strong elements. Then give your new span elements id values of xpText, healthText, and goldText, respectively.
+
+Your answer should follow this basic structure:
+
+<span class="stat">TEXT <strong><span id="VALUE">TEXT</span></strong></span>
+
+**/
+
+<span class="stat">XP: <strong><span id="xpText">0</span></strong></span>
+<span class="stat">Health: <strong><span id="healthText">100</span></strong></span>
+<span class="stat">Gold: <strong><span id="goldText">50</span></strong></span>
