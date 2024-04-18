f7f8c9f44f376aeb0e42222f4a890767268b4af3
Brian Grifinn <briangrifinn@gmail.com>
2024-04-18 22:26:44 +0200

    Add files via upload
    For now, you want the player to start with just the "stick".
         Change the inventory array to have "stick" as its only value.

---
+++ freecodecam7.js
@@ -0,0 +1,12 @@
+/**
+    For now, you want the player to start with just the "stick".
+     Change the inventory array to have "stick" as its only value.
+**/
+
+let xp = 0;
+let health = 100;
+let gold = 50;
+let currentWeapon = 0;
+let fighting;
+let monsterHealth;
+let inventory = ["stick"];
